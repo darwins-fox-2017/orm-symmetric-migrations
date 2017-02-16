1e11e6c88b9a9d7d65df6132b904ec129aa8148b
hepiska <ego1403@gmail.com>
2017-02-16 22:21:29 +0700

    orm student done

---
+++ migrations/20170216151541-remove lastname.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return queryInterface.removeColumn('Students','lastname')
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.addColumn('Students','lastname',Sequelize.STRING)
+  }
+};
