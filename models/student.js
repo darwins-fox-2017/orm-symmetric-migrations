acc3c6a5183cfc828dca4456efc0372ae6dec0a3
mfpebrizal <ftm.pebrizal@gmail.com>
2017-02-19 19:02:28 +0700

    almost

--- models/student.js
+++ models/student.js
@@ -2,11 +2,17 @@
 module.exports = function(sequelize, DataTypes) {
   var Student = sequelize.define('Student', {
     first_name: DataTypes.STRING,
-    last_name: DataTypes.STRING
+    last_name: DataTypes.STRING,
+    name: DataTypes.STRING
   }, {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+      }
+    },
+    instanceMethods: {
+      getFullName : function () {
+        return `${this.first_name} ${this.last_name}`
       }
     }
   });
