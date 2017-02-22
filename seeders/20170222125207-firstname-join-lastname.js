0fa08f6491614ca5b8b0a71d83111e2bd5b875ea
wahyuhidayatt <wahyuhdyt95@gmail.com>
2017-02-22 21:15:07 +0700

    orm-symmetric-migration

---
+++ seeders/20170222125207-firstname-join-lastname.js
@@ -0,0 +1,38 @@
+'use strict';
+const models = require('../models');
+
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+
+    return new Promise((rej,res) => {
+      models.Student.findAll({
+        attributes : ["id","first_name","last_name"]
+      }).then(function(data){
+        var willGotoPromiseAll = data.map((row)=>{
+          return new Promise((resolve,reject)=>{
+            models.Student.update(
+              {name : row.getFullName()},
+              {where : {id : row.id}}
+            )
+          }).then(()=>{
+              resolve(data)
+          }).catch((err)=>{
+              reject(err)
+          })
+        })
+        Promise.all(willGotoPromiseAll).then(()=>{
+          console.log("success");
+          res()
+        }).catch((err)=>{
+          rej(err)
+        })
+      })
+    })
+
+  },
+
+  down: function (queryInterface, Sequelize) {
+
+  }
+};
